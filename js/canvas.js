cfbd9b066077cc9da28ef15aa639d1d2cdad762d
Beeckaa <rebeckalindskog@gmail.com>
2019-09-23 10:36:18 +0200

    Touch interaction for drawing application

--- js/canvas.js
+++ js/canvas.js
@@ -1,4 +1,5 @@
 var canvas = document.getElementById('canvas');
+var statusdiv = document.getElementById('statusdiv')
 var context = canvas.getContext('2d');
 
 var radius = 2;
@@ -12,7 +13,7 @@ canvas.height = window.innerHeight;
 context.lineWidth = radius * 2;
 
 var drawing = function(e){
-	if(dragging){
+	if(dragging) {
 		context.lineTo(e.offsetX, e.offsetY);
 		context.stroke();
 		context.beginPath();
@@ -37,3 +38,32 @@ var stopDraw = function(){
 canvas.addEventListener('mousedown', startDraw);
 canvas.addEventListener('mousemove', drawing);
 canvas.addEventListener('mouseup', stopDraw);
+
+// Eventlistener and function for touchstart
+canvas.addEventListener('touchstart', function(e){
+    dragging = true;
+    e.preventDefault()
+}, false);
+
+// Eventlistener and function for touchmove
+canvas.addEventListener('touchmove', function(e){
+    var touchobj = e.changedTouches[0] // Reference first touch point for this event
+    if(dragging) {
+		context.lineTo(touchobj.clientX, touchobj.clientY);
+		context.stroke();
+		context.beginPath();
+		context.arc(touchobj.clientX, touchobj.clientY, radius, start, end);
+		context.fill();
+		context.beginPath();
+		context.moveTo(touchobj.clientX, touchobj.clientY);
+	}
+    e.preventDefault()
+}, false);
+
+// Eventlistener and function for touchend
+canvas.addEventListener('touchend', function(e){
+    dragging = false;
+	context.beginPath();
+    e.preventDefault()
+}, false);
+
