4ad4bf32da6f40571989806ec2ea6f480e91062a
Otto Ortega <ottor058@student.liu.se>
2019-10-01 10:36:16 +0200

    Separated the data into training and test, and shuffled the order

--- dataprocessing.py
+++ dataprocessing.py
@@ -1,6 +1,8 @@
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.utils import shuffle
 
 
 # Load data (numpy 28x28 bitmaps)
@@ -16,39 +18,43 @@ label_turtle = ['turtle'] * len(data_turtle)
 
 #Separating training data and testing data
 
+#bird
+bird_x_train, bird_x_test = data_bird[:20000], data_bird[20000:25000]
+bird_y_train, bird_y_test = label_bird[:20000], label_bird[20000:25000]
 
-# bird_train, bird_test = data_bird[:60000], data_bird[60000:70000]
-# y_train, y_test = y[:60000], y[60000:70000]
+#sheep
+sheep_x_train, sheep_x_test = data_sheep[:20000], data_sheep[20000:25000]
+sheep_y_train, sheep_y_test = label_sheep[:20000], label_sheep[20000:25000]
 
-# X_train, X_test = X[:60000], X[60000:70000]
-# y_train, y_test = y[:60000], y[60000:70000]
+#turtle
+turtle_x_train, turtle_x_test = data_turtle[:20000], data_turtle[20000:25000]
+turtle_y_train, turtle_y_test = label_turtle[:20000], label_turtle[20000:25000]
 
-# X_train, X_test = X[:60000], X[60000:70000]
-# y_train, y_test = y[:60000], y[60000:70000]
 
-print('Bird data length: ', len(data_bird))
-print('Bird label data length: ', len(label_bird))
-print('Sheep data length: ', len(data_sheep))
-print('Sheep label data length: ', len(label_sheep))
-print('Turtle data length: ', len(data_turtle))
-print('Turtle label data length: ', len(label_turtle))
+# concatenate
+x_train = np.concatenate([bird_x_train[:],sheep_x_train[:], turtle_x_train[:]])
+y_train = np.concatenate([bird_y_train[:],sheep_y_train[:], turtle_y_train[:]])
 
-index_start = 10000 # <--- Change this for a different doodle
-img1 = data_bird[index_start]
-img2 = data_sheep[index_start]
-img3 = data_turtle[index_start]
+x_test = np.concatenate([bird_x_test[:],sheep_x_test[:], turtle_x_test[:]])
+y_test = np.concatenate([bird_y_test[:],sheep_y_test[:], turtle_y_test[:]])
 
-# plt.imshow(img1.reshape((28,28)))
-# plt.show()
+#Shuffle the order
+x_train, y_train = shuffle(x_train, y_train)
+
+########################################################################################
+####################################  Plotting #########################################
+########################################################################################
+
+index_start = 1000 # <--- Change this for a different doodle
+img1 = x_train[index_start]
+# img2 = data_sheep[index_start]
+# img3 = data_turtle[index_start]
+
+plt.imshow(img1.reshape((28,28)))
+plt.title(y_train[index_start])
+plt.show()
 # plt.imshow(img2.reshape((28,28)))
 # plt.show()
 # plt.imshow(img3.reshape((28,28)))
 # plt.show()
 
-
-# concatinate
-
-# print(len(data))
-# datatot = pd.concat([data[:],data[:]])
-
-# print(len(datatot))
