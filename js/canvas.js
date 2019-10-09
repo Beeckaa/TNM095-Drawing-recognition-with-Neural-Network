a956b5bde8124ac2e98be5e09844d8a62b31101b
Beeckaa <rebeckalindskog@gmail.com>
2019-10-09 22:00:34 +0200

    Inverting the image data in preprocessing so that we could have a white canvas with black strokes

--- js/canvas.js
+++ js/canvas.js
@@ -1,7 +1,7 @@
 // Variables related to the canvas drawing
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
-var radius = 12;
+var radius = 10;
 var start = 0;
 var end = Math.PI * 2;
 var dragging = false;
@@ -11,8 +11,8 @@ canvas.height = 400;
 context.lineWidth = radius * 2;
 var rect = canvas.getBoundingClientRect();
 
-// Fill canvas with black background color
-context.fillStyle = '#000';
+// Fill canvas with white background color
+context.fillStyle = '#fff';
 context.fillRect(0, 0, canvas.width, canvas.height);
 
 // Keep track of min/max for each axis
@@ -49,7 +49,7 @@ var clearCanvas = function() {
 	maxY = 0;
     context.clearRect(0, 0, canvas.width, canvas.height);
 
-	context.fillStyle = '#000';
+	context.fillStyle = '#fff';
 	context.fillRect(0,0,canvas.width, canvas.height);
 	document.getElementById('speech-bubble').innerHTML = "...";
 }
@@ -59,8 +59,8 @@ var clearCanvas = function() {
  */
 var drawing = function(e){
 	if(dragging) {
-		context.strokeStyle = '#fff';
-		context.fillStyle = '#fff';
+		context.strokeStyle = '#000';
+		context.fillStyle = '#000';
 		context.lineTo(e.offsetX, e.offsetY);
 		context.stroke();
 		context.beginPath();
@@ -112,10 +112,10 @@ async function predict() {
 	// Get the image data from the canvas with boundingbox
     const imgData = getImageData();
 
-	// Draw imgData on canvas (remove this later)
-	//context.fillStyle = '#fff';
-	//context.fillRect(0, 0, canvas.width, canvas.height);
-	//context.putImageData(imgData, 100, 100);
+	/*// Draw imgData on canvas (remove this later)
+	context.fillStyle = '#000';
+	context.fillRect(0, 0, canvas.width, canvas.height);
+	context.putImageData(imgData, 100, 100);*/
 
 	// Do the prediction and preprocess the canvas
 	var predictions = await net.predict(preprocessCanvas(imgData));
@@ -189,8 +189,8 @@ canvas.addEventListener('touchstart', function(e){
 canvas.addEventListener('touchmove', function(e){
     var touchobj = e.changedTouches[0] // Reference first touch point for this event
     if(dragging) {
-		context.strokeStyle = '#fff';
-		context.fillStyle = '#fff';
+		context.strokeStyle = '#000';
+		context.fillStyle = '#000';
 		context.lineTo(touchobj.clientX, touchobj.clientY);
 		context.stroke();
 		context.beginPath();
@@ -268,6 +268,24 @@ function getImageData() {
 		maxX = maxY;
 	}
 	const imgData = context.getImageData(minX, minY, maxX, maxY);
+	var dataArr = imgData.data;
+
+	// Inverting the colors in imgData.
+	for(var i = 0; i < dataArr.length; i += 4) {
+		var r = dataArr[i]; 	// Red color lies between 0 and 255
+		var g = dataArr[i + 1]; // Green color lies between 0 and 255
+		var b = dataArr[i + 2]; // Blue color lies between 0 and 255
+		var a = dataArr[i + 3]; // Transparency lies between 0 and 255
+
+		var invertedRed = 255 - r;
+		var invertedGreen = 255 - g;
+		var invertedBlue = 255 - b;
+
+		dataArr[i] = invertedRed;
+		dataArr[i + 1] = invertedGreen;
+		dataArr[i + 2] = invertedBlue;
+	}
+
 	return imgData;
 }
 
